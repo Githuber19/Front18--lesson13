c9081f5fbfb4731ea22218bfeaa25e5559a0a7a4
Levan Natadze <98254494+Githuber19@users.noreply.github.com>
2022-11-15 16:18:24 +0400

    Update

---
+++ app.js
@@ -0,0 +1,29 @@
+let userName;
+let userHobby;
+let ueserAdress;
+let userEmail;
+let yearOfBirth;
+
+let myName = "LEVANI";
+let YearOfBirth = 1998;
+let currentYear = 2022;
+let myHobby = "FOOTBALL GAME";
+
+console.log(myName);
+console.log(YearOfBirth);
+console.log(currentYear);
+console.log(myHobby);
+
+console.log(
+  `my name is ${myName} i am ${
+    currentYear - YearOfBirth
+  } years old, my hobby is ${myHobby}`
+);
+console.log(
+  "My name is " +
+    myName +
+    " i am " +
+    (currentYear - YearOfBirth) +
+    " yaers old, my hobby is " +
+    myHobby
+);
